09e4300651dbae3b6412ee96ecb779bdedd86a63
Mehuli Pal <54908756+Mehulipal@users.noreply.github.com>
2021-11-30 04:09:06 +0530

    add

---
+++ MarketShop/admin/admin_shopDelete.php
@@ -0,0 +1,74 @@
+<?php
+// Process delete operation after confirmation
+if(isset($_POST["SID"]) && !empty($_POST["SID"])){
+    // Include config file
+    require_once (dirname(__FILE__)."/../config.php");
+    
+    // Prepare a delete statement to delete shop
+    $sql = "DELETE FROM shop WHERE SID = ?";
+    
+    if($stmt = mysqli_prepare($conn, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "s", $param_SID);
+        
+        // Set parameters
+        $param_SID = trim($_POST["SID"]);
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            // Records deleted successfully. Redirect to home page
+            header("location: admin_home.php");
+            exit();
+        } else{
+            echo '<script>alert("Oops! Something went wrong. Please try again later.")</script>';
+        }
+    }
+     
+    // Close statement
+    mysqli_stmt_close($stmt);
+    
+    // Close connection
+    mysqli_close($conn);
+} else{
+    // Check existence of SID parameter
+    if(empty(trim($_GET["SID"]))){
+        echo '<script>alert("Could not delete shop")</script>';
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Delete Record</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-5 mb-3">Delete Shop</h2>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="alert alert-danger">
+                            <input type="hidden" name="SID" value="<?php echo trim($_GET["SID"]); ?>"/>
+                            <p>Are you sure you want to delete this shop?</p>
+                            <p>
+                                <input type="submit" value="Yes" class="btn btn-danger">
+                                <a href="admin_home.php" class="btn btn-secondary">No</a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
